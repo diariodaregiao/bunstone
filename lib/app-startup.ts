d174021293d625dd5353f5ef686c7242bec144d4
Gabriel Alves <ga654066@gmail.com>
2025-12-30 17:53:10 -0300

    feat:  interact with and execute timeout providers

--- lib/app-startup.ts
+++ lib/app-startup.ts
@@ -1,25 +1,23 @@
 import Elysia from "elysia";
-import { Logger } from "./utils/logger";
 import { processParameters } from "./http-params";
+import { Logger } from "./utils/logger";
 
 export class AppStartup {
   private static readonly elysia: Elysia = new Elysia();
   private static readonly logger = new Logger(AppStartup.name);
 
   static create(module: any) {
-    const controllers: Map<
-      any,
-      { httpMethod: string; pathname: string; methodName: string }[]
-    > = Reflect.getMetadata("dip:module:routes", module);
+    const controllers: Map<any, { httpMethod: string; pathname: string; methodName: string }[]> = Reflect.getMetadata(
+      "dip:module:routes",
+      module,
+    );
 
     for (const item of controllers.entries()) {
       const [controllerInstance, methods] = item;
       const controller = new controllerInstance();
 
       for (const method of methods) {
-        AppStartup.logger.log(
-          `Registering ${method.httpMethod} route: ${method.pathname}`
-        );
+        AppStartup.logger.log(`Registering ${method.httpMethod} route: ${method.pathname}`);
         const httpMethod = method.httpMethod.toLowerCase();
         if (!(httpMethod in AppStartup.elysia)) {
           throw new Error(`HTTP method ${method.httpMethod} is not supported.`);
@@ -27,21 +25,32 @@ export class AppStartup {
 
         AppStartup.elysia[httpMethod as keyof Elysia](
           method.pathname,
-          (req: any) =>
-            AppStartup.executeControllerMethod(
-              req,
-              controller,
-              method.methodName
-            ),
+          (req: any) => AppStartup.executeControllerMethod(req, controller, method.methodName),
           {
             beforeHandle(req: any) {
-              console.log("headers", req.headers);
+              console.log(`Incoming request: ${req.method} ${req.path}`);
             },
-          }
+          },
         );
       }
     }
 
+    const providersTimeouts: Map<any, { delay: number; methodName: string }[]> = Reflect.getMetadata(
+      "dip:module:providers:timeouts",
+      module,
+    );
+
+    for (const item of providersTimeouts.entries()) {
+      const [providerInstance, timeouts] = item;
+      const provider = new providerInstance();
+
+      for (const timeout of timeouts) {
+        AppStartup.logger.log(`Scheduling timeout for method: ${timeout.methodName} with delay: ${timeout.delay}ms`);
+        setTimeout(() => {
+          provider[timeout.methodName]();
+        }, timeout.delay);
+      }
+    }
     return {
       listen: this.listen,
     };
@@ -52,11 +61,7 @@ export class AppStartup {
     AppStartup.elysia.listen(port);
   }
 
-  private static async executeControllerMethod(
-    req: any,
-    controller: any,
-    method: any
-  ) {
+  private static async executeControllerMethod(req: any, controller: any, method: any) {
     const args = processParameters(req, controller, method);
     return controller[method](...args);
   }
--- lib/module.ts
+++ lib/module.ts
@@ -11,19 +11,13 @@ export function Module(moduleConfig: ModuleConfig = {}): ClassDecorator {
   moduleConfig.imports = moduleConfig.imports || [];
   moduleConfig.exports = moduleConfig.exports || [];
 
-  const controllers = new Map<
-    any,
-    { httpMethod: string; pathname: string; methodName: string }[]
-  >();
+  const controllers = new Map<any, { httpMethod: string; pathname: string; methodName: string }[]>();
 
   for (const controller of moduleConfig.controllers) {
     controllers.set(controller, []);
 
     for (const controllerSymbol of Object.getOwnPropertySymbols(controller)) {
-      const controllerPathname = Reflect.getOwnMetadata(
-        "dip:controller:pathname",
-        controller
-      );
+      const controllerPathname = Reflect.getOwnMetadata("dip:controller:pathname", controller);
       const controllerMethods: {
         httpMethod: string;
         pathname: string;
@@ -31,9 +25,7 @@ export function Module(moduleConfig: ModuleConfig = {}): ClassDecorator {
       }[] = controller[controllerSymbol];
 
       controllerMethods.forEach((cm) => {
-        const pathname = `${
-          controllerPathname === "/" ? "" : controllerPathname
-        }${cm.pathname}`;
+        const pathname = `${controllerPathname === "/" ? "" : controllerPathname}${cm.pathname}`;
 
         controllers.get(controller)?.push({
           httpMethod: cm.httpMethod,
@@ -44,8 +36,28 @@ export function Module(moduleConfig: ModuleConfig = {}): ClassDecorator {
     }
   }
 
+  const providersTimeouts = new Map<any, { delay: number; methodName: string }[]>();
+
+  for (const provider of moduleConfig.providers) {
+    for (const providerSymbol of Object.getOwnPropertySymbols(provider.prototype)) {
+      const providerType: string = provider.prototype[providerSymbol].type;
+
+      if (providerType === "timeout") {
+        if (!providersTimeouts.has(provider)) {
+          providersTimeouts.set(provider, []);
+        }
+
+        providersTimeouts.get(provider)?.push({
+          delay: provider.prototype[providerSymbol].delay,
+          methodName: provider.prototype[providerSymbol].methodName,
+        });
+      }
+    }
+  }
+
   return function (target) {
     Reflect.defineMetadata("dip:module", "is_module", target);
     Reflect.defineMetadata("dip:module:routes", controllers, target);
+    Reflect.defineMetadata("dip:module:providers:timeouts", providersTimeouts, target);
   };
 }
