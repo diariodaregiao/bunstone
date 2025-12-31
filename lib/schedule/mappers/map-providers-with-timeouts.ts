4cc57d868a14176ae9f468c916683f277735ad60
Gabriel Alves <ga654066@gmail.com>
2025-12-31 08:57:15 -0300

    feat: add map providers with timeout

---
+++ lib/schedule/mappers/map-providers-with-timeouts.ts
@@ -0,0 +1,34 @@
+import type { ModuleConfig } from "../../types/module-config";
+
+export class MapProvidersWithTimeout {
+  static execute(providers: ModuleConfig["providers"] = []) {
+    return this.mapManyProviders(providers);
+  }
+
+  private static mapManyProviders(
+    providers: ModuleConfig["providers"] = [],
+  ): Map<any, { delay: number; methodName: string }[]> {
+    const providersTimeouts = new Map<any, { delay: number; methodName: string }[]>();
+
+    for (const provider of providers) {
+      for (const providerSymbol of Object.getOwnPropertySymbols(provider.prototype)) {
+        const methods = provider.prototype[providerSymbol];
+
+        for (const method of methods) {
+          if (method.type === "timeout") {
+            if (!providersTimeouts.has(provider)) {
+              providersTimeouts.set(provider, []);
+            }
+          }
+
+          providersTimeouts.get(provider)?.push({
+            delay: method.delay,
+            methodName: method.methodName,
+          });
+        }
+      }
+    }
+
+    return providersTimeouts;
+  }
+}
