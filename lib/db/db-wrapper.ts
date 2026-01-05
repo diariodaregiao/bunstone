e4ff4154c665b2efeba1d4c8f2e78a67ca4f8d7b
Gabriel Alves <ga654066@gmail.com>
2026-01-05 10:38:51 -0300

    feat: update method transaction and add doc in methods

--- lib/db/db-wrapper.ts
+++ lib/db/db-wrapper.ts
@@ -3,6 +3,8 @@ import { SQL } from "bun";
 /**
  * Creates a new database wrapper instance.
  * @param connectionString - The database connection string, in the format:
+ * 
+ * @example
  * "mysql://user:password@host:port/database"
  */
 export class DbWrapper {
@@ -22,19 +24,31 @@ export class DbWrapper {
 
   /**
    * Method to execute a query against the database.
-   * @param queryString 
+   * @param queryString The SQL query string to be executed.
+   * @example
+   *  const users = await db.query("SELECT * FROM users");
    * @returns Array with the query results.
    */
   async query<T = any>(queryString: string): Promise<T[]> {
-    const res =  await this._mysql.unsafe<T>(queryString) as T[];
-    return res
+    const res = (await this._mysql.unsafe<T>(queryString)) as T[];
+    return res;
   }
 
-  async transaction(queries: ((trx: SQL) => Promise<void>)[]): Promise<void> {
+  /**
+   * 
+   * @param callback Function that receives a array of queries to be executed in a transaction
+   * 
+   * @example
+   *   await this.db.transaction(async (trx) => {
+          await trx.unsafe("INSERT INTO users VALUES (UUID(), 'Joe', 'Doe', 25 )");
+          await trx.unsafe("UPDATE users SET age = age + 1 WHERE name != 'Joe'")
+        });
+   * @return Promise that resolves when the transaction is complete 
+   * 
+   */
+  async transaction(callback: (trx: SQL) => Promise<void>): Promise<void> {
     await this._mysql.transaction(async (trx) => {
-      for (const query of queries) {
-        await query(trx);
-      }
+      await callback(trx);
     });
   }
 }
